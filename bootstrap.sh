bb67e2ce9e746915c179560d13000b1cfb5cd486
José Luis Campaña Pérez <98171554+josecampana@users.noreply.github.com>
2025-11-20 15:43:19 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-
 set -e
 
 TMP=$(mktemp -d)
 cd "$TMP"
 
-echo "Descargando herramientas de instalación..."
-curl -L https://github.com/tuusuario/osx-dev-packages/archive/refs/heads/main.zip -o repo.zip
+echo "downloading osx-dev-packages installation tool"
 
-unzip repo.zip >/dev/null
+curl -L \
+  https://github.com/josecampana/osx-dev-packages/archive/refs/heads/main.zip \
+  -o repo.zip
+
+echo "unzipping..."
+unzip -q repo.zip
+
 cd osx-dev-packages-main
 
 chmod +x install.sh
